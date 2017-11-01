8e0678c0d2819224722ce6f952b58c006748d763
Tim <tspengler2@gmail.com>
2017-10-31 20:26:01 -0700

    First commit

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: 'dashboard', title: 'Weather',  icon: 'cloud', class: '' },
+    { path: 'user-profile', title: 'Roles',  icon:'person', class: '' },
+    { path: 'table-list', title: 'Schedule',  icon:'content_paste', class: '' },
+    { path: 'typography', title: 'Manual Operations',  icon:'settings', class: '' },
+    { path: 'icons', title: 'Events',  icon:'alarm', class: '' },
+    { path: 'maps', title: 'Maps',  icon:'location_on', class: '' },
+    { path: 'notifications', title: 'Notifications',  icon:'notifications', class: '' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
